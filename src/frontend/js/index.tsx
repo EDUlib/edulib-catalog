3b3d09b0c2cd84a712397dc35dfcbb2d7d886cf8
Truong David <david.truong@edulib.org>
2020-01-14 16:09:42 -0500

    💄(edulib) update theme to richie 1.14
    update theme to richie 1.14 but maintain font icons

--- src/frontend/js/index.tsx
+++ src/frontend/js/index.tsx
@@ -23,17 +23,20 @@ import startCase from 'lodash-es/startCase';
 // Import the top-level components that can be directly called from the CMS
 import { RootSearchSuggestField } from 'components/RootSearchSuggestField';
 import { Search } from 'components/Search';
+import { UserLogin } from 'components/UserLogin';
 import { handle } from 'utils/errors/handle';
 // List them in an interface for type-safety when we call them. This will let us use the props for
 // any top-level component in a way TypeScript understand and accepts
 interface ComponentLibrary {
   Search: typeof Search;
   RootSearchSuggestField: typeof RootSearchSuggestField;
+  UserLogin: typeof UserLogin;
 }
 // Actually create the component map that we'll use below to access our component classes
 const componentLibrary: ComponentLibrary = {
   RootSearchSuggestField,
   Search,
+  UserLogin,
 };
 // Type guard: ensures a given string (candidate) is indeed a proper key of the componentLibrary with a corresponding
 // component. This is a runtime check but it allows TS to check the component prop types at compile time
@@ -44,49 +47,62 @@ function isComponentName(
 }
 
 // Wait for the DOM to load before we scour it for an element that requires React to be rendered
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('DOMContentLoaded', async event => {
   // Find all the elements that need React to render a component
-  Array.prototype.forEach.call(
-    document.querySelectorAll('.fun-react'),
-    async (element: Element) => {
+  const richieReactSpots = Array.prototype.slice.call(
+    document.querySelectorAll('.richie-react'),
+  );
+
+  // Only move on with anything if there are actually components to render
+  if (richieReactSpots.length) {
+    // Determine the BCP47/RFC5646 locale to use
+    const locale = document.querySelector('html')!.getAttribute('lang');
+
+    if (!locale) {
+      throw new Error(
+        '<html> lang attribute is required to be set with a BCP47/RFC5646 locale.',
+      );
+    }
+
+    // Only load Intl polyfills & pre-built locale data for browsers that need it
+    try {
+      if (!Intl.PluralRules) {
+        await import('intl-pluralrules');
+      }
+      // TODO: remove type assertion when typescript libs include RelativeTimeFormat
+      if (!(Intl as any).RelativeTimeFormat) {
+        await import('@formatjs/intl-relativetimeformat');
+        // Polyfilled locale data is keyed by 2-letter language code
+        let languageCode = locale;
+        if (languageCode.match(/^.*_.*$/)) {
+          languageCode = locale.split('_')[0];
+        }
+        // Get `react-intl`/`formatjs` lang specific parameters and data
+        await import(
+          `@formatjs/intl-relativetimeformat/dist/locale-data/${languageCode}`
+        );
+      }
+    } catch (e) {
+      handle(e);
+    }
+
+    // Load our own strings for the given lang
+    let translatedMessages: any = null;
+    try {
+      translatedMessages = await import(`./translations/${locale}.json`);
+    } catch (e) {
+      handle(e);
+    }
+
+    richieReactSpots.forEach((element: Element) => {
       // Generate a component name. It should be a key of the componentLibrary object / ComponentLibrary interface
       const componentName = startCase(
-        get(element.className.match(/fun-react--([a-zA-Z-]*)/), '[1]') || '',
+        get(element.className.match(/richie-react--([a-zA-Z-]*)/), '[1]') || '',
       )
         .split(' ')
         .join('');
       // Sanity check: only attempt to access and render components for which we do have a valid name
       if (isComponentName(componentName)) {
-        // Determine a lang (localeCode) based on the `data-locale` attribute
-        const locale = element.getAttribute('data-locale') || 'en';
-        let localeCode = locale;
-        if (localeCode.match(/^.*_.*$/)) {
-          localeCode = locale.split('_')[0];
-        }
-
-        // Only load Intl polyfills & pre-built locale data for browsers that need it
-        try {
-          if (!Intl.PluralRules) {
-            await import('intl-pluralrules');
-          }
-          // TODO: remove type assertion when typescript libs include RelativeTimeFormat
-          if (!(Intl as any).RelativeTimeFormat) {
-            await import('@formatjs/intl-relativetimeformat');
-            // Get `react-intl`/`formatjs` lang specific parameters and data
-            await import(
-              `@formatjs/intl-relativetimeformat/dist/locale-data/${localeCode}`
-            );
-          }
-        } catch (e) {
-          handle(e);
-        }
-
-        // Load our own strings for the given lang
-        let translatedMessages = null;
-        try {
-          translatedMessages = await import(`./translations/${locale}.json`);
-        } catch (e) {}
-
         // Do get the component dynamically. We know this WILL produce a valid component thanks to the type guard
         const Component = componentLibrary[componentName];
 
@@ -96,7 +112,7 @@ document.addEventListener('DOMContentLoaded', event => {
 
         // Render the component inside an `IntlProvider` to be able to access translated strings
         ReactDOM.render(
-          <IntlProvider locale={localeCode} messages={translatedMessages}>
+          <IntlProvider locale={locale} messages={translatedMessages}>
             <Component {...props} />
           </IntlProvider>,
           element,
@@ -108,6 +124,6 @@ document.addEventListener('DOMContentLoaded', event => {
             componentName,
         );
       }
-    },
-  );
+    });
+  }
 });
