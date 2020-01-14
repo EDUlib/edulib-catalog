7202f226a595fa2f324d9c78e8e2473da7c140d8
Truong David <david.truong@edulib.org>
2020-01-14 14:43:31 -0500

    💄(edulib) fork theme
    add frontend files to fork from fun-mooc

---
+++ src/frontend/js/components/SearchFilterGroup/SearchFilterGroup.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { FilterDefinition } from '../../types/filters';
+import { SearchFilterGroupModal } from '../SearchFilterGroupModal';
+import { SearchFilterValueLeaf } from '../SearchFilterValueLeaf/SearchFilterValueLeaf';
+import { SearchFilterValueParent } from '../SearchFilterValueParent/SearchFilterValueParent';
+
+export interface SearchFilterGroupProps {
+  filter: FilterDefinition;
+}
+
+export const SearchFilterGroup = ({ filter }: SearchFilterGroupProps) => (
+  <fieldset className="search-filter-group">
+    <legend className="search-filter-group__title">{filter.human_name}</legend>
+    <div className="search-filter-group__list">
+      {filter.values.map(value =>
+        value.key.startsWith(
+          'P-',
+        ) /* Values with children have a key that starts with `P-` by convention */ ? (
+          <SearchFilterValueParent
+            filter={filter}
+            value={value}
+            key={value.key}
+          />
+        ) : (
+          /* Other values' keys start with `L-` */ <SearchFilterValueLeaf
+            filter={filter}
+            value={value}
+            key={value.key}
+          />
+        ),
+      )}
+    </div>
+    {filter.has_more_values && filter.is_searchable ? (
+      <SearchFilterGroupModal filter={filter} />
+    ) : null}
+  </fieldset>
+);
