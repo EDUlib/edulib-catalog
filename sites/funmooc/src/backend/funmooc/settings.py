36810676ccbc704860caf2311dc026e2d2f60f41
Truong David <david.truong@edulib.org>
2020-11-30 11:29:51 -0500

    🔧(edulib) swap simpletextplugin for CKEditorPlugin in CMS_PLACEHOLDER_CONF settings

--- sites/funmooc/src/backend/funmooc/settings.py
+++ sites/funmooc/src/backend/funmooc/settings.py
@@ -400,8 +400,8 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
         },
         "courses/cms/course_detail.html course_introduction": {
             "name": _("Catch phrase"),
-            "plugins": ["PlainTextPlugin"],
-            "limits": {"PlainTextPlugin": 1},
+            "plugins": ["CKEditorPlugin"],
+            "limits": {"CKEditorPlugin": 1},
         },
         "courses/cms/course_detail.html course_teaser": {
             "name": _("Teaser"),
@@ -561,8 +561,8 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
         },
         "courses/cms/blogpost_detail.html excerpt": {
             "name": _("Excerpt"),
-            "plugins": ["PlainTextPlugin"],
-            "limits": {"PlainTextPlugin": 1},
+            "plugins": ["CKEditorPlugin"],
+            "limits": {"CKEditorPlugin": 1},
         },
         "courses/cms/blogpost_detail.html body": {
             "name": _("Body"),
@@ -581,8 +581,8 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
         },
         "courses/cms/program_detail.html program_excerpt": {
             "name": _("Excerpt"),
-            "plugins": ["PlainTextPlugin"],
-            "limits": {"PlainTextPlugin": 1},
+            "plugins": ["CKEditorPlugin"],
+            "limits": {"CKEditorPlugin": 1},
         },
         "courses/cms/program_detail.html program_body": {
             "name": _("Body"),
